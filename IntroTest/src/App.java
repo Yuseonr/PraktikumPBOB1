2ffa98bbf6c4d937ab06bdd0f3e2f1dc8665a18c
Yuseonr <rafianandra@gmail.com>
2026-02-19 19:11:08 +0700

    test commit pertama

---
+++ IntroTest/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+// javac -d bin ./src/App.java
+// java -cp bin App
